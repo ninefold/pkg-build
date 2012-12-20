bd6ba6efb01f556b17c613f37b213bb0bd728c9a
Chris Roberts <chrisroberts.code@gmail.com>
2012-12-20 10:21:05 -0800

    Reload block that is a mostly no-op for now

--- recipes/deps.rb
+++ recipes/deps.rb
@@ -1,7 +1,18 @@
 # These are all the things we want all the time. So load them!
 
+# This is currently ineffective, but left because it should be soon
+ruby_block 'reload paths on new ruby' do
+  block do
+    node.default[:pkg_build][:gems][:exec] = File.join(node.languages.ruby.bin_dir, 'gem')
+    node.default[:pkg_build][:passenger][:ruby_bin] = node.languages.ruby.bin_dir
+    node.default[:pkg_build][:passenger][:root] = File.dirname(node.languages.ruby.bin_dir)
+  end
+  action :nothing
+  subscribes :create, 'ohai[ruby]', :immediately
+end
+
 include_recipe 'builder'
 include_recipe 'fpm-tng'
 include_recipe 'reprepro'
 
-node.default[:pkg_build][:builder] = true
+node.set[:pkg_build][:builder] = true
