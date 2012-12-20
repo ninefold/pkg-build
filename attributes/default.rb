04b4ccde112a850a8beee71f395a6c46532ba3ef
Chris Roberts <chrisroberts.code@gmail.com>
2012-12-20 10:20:41 -0800

    Base default ruby paths on ohai available ruby

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,10 +1,11 @@
 default[:pkg_build][:builder] = false
 default[:pkg_build][:pkg_prefix] = 'hw'
 default[:pkg_build][:use_pkg_build_ruby] = false
-default[:pkg_build][:passenger][:ruby_bin] = RbConfig::CONFIG['bindir']
+default[:pkg_build][:gems][:exec] = File.join(node.languages.ruby.bin_dir, 'gem')
+default[:pkg_build][:passenger][:ruby_bin] = node.languages.ruby.bin_dir
+default[:pkg_build][:passenger][:root] = File.dirname(node.languages.ruby.bin_dir)
 default[:pkg_build][:passenger][:version] = '3.0.18'
 default[:pkg_build][:passenger][:ruby_dependency] = nil
-default[:pkg_build][:gems][:exec] = File.join(RbConfig::CONFIG['bindir'], 'gem')
 default[:pkg_build][:ruby][:uri_base] = 'http://ftp.ruby-lang.org/pub/ruby/stable/'
 default[:pkg_build][:ruby][:version] = '1.9.3'
 default[:pkg_build][:ruby][:patchlevel] = 'p327'
