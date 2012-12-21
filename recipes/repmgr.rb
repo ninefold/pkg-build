1d03e6a71c50351dd076af72547b623892cc217c
Chris Roberts <chrisroberts.code@gmail.com>
2012-12-21 07:48:22 -0800

    Update repmgr recipe details

--- recipes/repmgr.rb
+++ recipes/repmgr.rb
@@ -20,9 +20,9 @@ r_url = File.join(node[:pkg_build][:repmgr][:base_uri], "repmgr-#{node[:pkg_buil
 repmgr_postinst = File.join(node[:builder][:build_dir], "repmgr-#{node[:pkg_build][:repmgr][:version]}", 'postinst')
 
 builder_remote "repmgr-#{node[:pkg_build][:repmgr][:version]}" do
-  remote_file r_rul
+  remote_file r_url
   suffix_cwd "repmgr-#{node[:pkg_build][:repmgr][:version]}"
-  commands "make USE_PGXS=1 install DESTDIR=$PKG_DIR"
+  commands ["make USE_PGXS=1 install DESTDIR=$PKG_DIR"]
 end
 
 template repmgr_postinst do
@@ -37,7 +37,7 @@ fpm_tng_package [node[:pkg_build][:prefix], 'repmgr'].compact.join('-') do
   output_type 'deb'
   description "PostgreSQL Replication Manager"
   version node[:pkg_build][:repmgr][:version]
-  chdir File.join(node[:builder][:packaging_dir], ruby_name)
+  chdir File.join(node[:builder][:packaging_dir], "repmgr-#{node[:pkg_build][:repmgr][:version]}")
   after_install repmgr_postinst
   depends %w(libpq-dev)
   reprepro true
