f9aad65bdb1e9b93c296da2b5a8d420fac04105e
Chris Roberts <chrisroberts.code@gmail.com>
2012-12-21 09:00:31 -0800

    Update sphinx building

--- attributes/sphinx.rb
+++ attributes/sphinx.rb
@@ -1,1 +1,2 @@
 default[:pkg_build][:sphinx][:version] = '2.0.6'
+default[:pkg_build][:sphinx][:build_dependencies] = %w(libmysqlclient-dev)
--- recipes/sphinx.rb
+++ recipes/sphinx.rb
@@ -1,14 +1,17 @@
 include_recipe 'pkg-build::deps'
 
+node[:pkg_build][:sphinx][:build_dependencies].each do |d_pkg|
+  package d_pkg
+end
+
 sphinx_name = "sphinx-#{node[:pkg_build][:sphinx][:version]}"
 
 builder_remote sphinx_name do
   remote_file "http://sphinxsearch.com/files/sphinx-#{node[:pkg_build][:sphinx][:version]}-release.tar.gz"
-  suffix_cwd "sphinx-#{node[:sphinx][:version]}-release"
-  depends %w(libc libexpat1 libgcc1 libmysqlclient18 libpq5 libstdc++6 libstemmer0d zlib1g)
+  suffix_cwd "#{sphinx_name}-release"
   commands [
     './configure --prefix=/usr/local',
-    'make'
+    'make',
     'make install DISTDIR=$PKG_DIR'
   ]
   # creates 
@@ -17,7 +20,8 @@ end
 fpm_tng_package 'sphinxsearch' do
   output_type 'deb'
   description 'Sphinx search'
-  version node[:sphinx][:version]
+  depends %w(libc6 libexpat1 libgcc1 libmysqlclient18 libpq5 libstdc++6 libstemmer0d zlib1g)
+  version node[:pkg_build][:sphinx][:version]
   chdir File.join(node[:builder][:packaging_dir], sphinx_name)
   reprepro node[:pkg_build][:reprepro]
 end
