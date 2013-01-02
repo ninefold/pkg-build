eac1c71e293ce3700fcdd643316e88a61c22a68e
Chris Roberts <chrisroberts.code@gmail.com>
2013-01-02 07:54:51 -0800

    Patch level bump information

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## v0.1.2
+* Allow ruby version suffix to be optional on package name
+
 ## v0.1.1
 * Update ruby build naming
 
--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Heavy Water'
 maintainer_email 'support@hw-ops.com'
 description 'Recipes for building packages'
 
-version '0.1.1'
+version '0.1.2'
 
 depends 'builder'
 depends 'fpm-tng'
