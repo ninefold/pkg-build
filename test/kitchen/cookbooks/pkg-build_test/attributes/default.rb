3f2de6d3a393934c241e8560ca813a162b54afa7
Chris Roberts <chrisroberts.code@gmail.com>
2013-01-11 14:41:59 -0800

    Add test kitchen support
    * testing for ruby build
    * testing for passenger build

---
+++ test/kitchen/cookbooks/pkg-build_test/attributes/default.rb
@@ -0,0 +1,2 @@
+default[:pkg_build][:reprepro] = false
+default[:pkg_build][:passenger][:allow_omnibus_chef_ruby] = true
---
+++ test/kitchen/cookbooks/pkg-build_test/metadata.rb
@@ -0,0 +1,4 @@
+name 'pkg-build_test'
+version '0.0.1'
+
+depends 'pkg-build'
