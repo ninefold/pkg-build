6bf006bc31b1d44e95f143f94af268356f0c530f
Chris Roberts <chrisroberts.code@gmail.com>
2013-01-11 14:38:41 -0800

    Keep node attribute access consistent

--- attributes/passenger.rb
+++ attributes/passenger.rb
@@ -1,4 +1,4 @@
-default[:pkg_build][:passenger][:root] = File.dirname(node.languages.ruby.bin_dir)
+default[:pkg_build][:passenger][:root] = File.dirname(node[:languages][:ruby][:bin_dir])
 default[:pkg_build][:passenger][:version] = '3.0.18'
 default[:pkg_build][:passenger][:ruby_dependency] = nil
 default[:pkg_build][:passenger][:allow_omnibus_chef_ruby] = false
