be0dffa36bae628965f44350b3203ce69ae5729c
Chris Roberts <chrisroberts.code@gmail.com>
2013-01-11 14:36:01 -0800

    Only install package remover script if reprepro is enabled

--- recipes/helpers.rb
+++ recipes/helpers.rb
@@ -1,5 +1,6 @@
-
-template '/usr/local/bin/pkg-build-remover' do
-  source 'pkg-build-remover.erb'
-  mode 0755
+if(node[:pkg_build][:reprepro])
+  template '/usr/local/bin/pkg-build-remover' do
+    source 'pkg-build-remover.erb'
+    mode 0755
+  end
 end
