b4b69da693de0219c64a6ef0222bdd0c55049f1a
Chris Roberts <chrisroberts.code@gmail.com>
2013-04-06 16:05:02 -0700

    Use attribute for dependency loading

--- definitions/ruby.rb
+++ definitions/ruby.rb
@@ -6,11 +6,7 @@ define :build_ruby, :version => nil, :patchlevel => nil, :repository => nil do
 
   include_recipe 'pkg-build::deps'
 
-  %w(
-    openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev 
-    libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev 
-    autoconf libc6-dev ncurses-dev automake libtool bison subversion pkg-config
-  ).each do |r_dep|
+  node[:pkg_build][:ruby][:package_dependencies].each do |r_dep|
     package r_dep
   end
 
