52db51f2550b17b99cfe71f1e2cf23c0b0afa210
Chris Roberts <chrisroberts.code@gmail.com>
2013-04-08 07:18:56 -0700

    Update required dependency version. Bump version for next release.

--- metadata.rb
+++ metadata.rb
@@ -3,14 +3,14 @@ maintainer 'Heavy Water'
 maintainer_email 'support@hw-ops.com'
 description 'Recipes for building packages'
 
-version '0.1.9'
+version '0.2.0'
 
 supports 'ubuntu'
 
 depends 'builder'
-depends 'fpm-tng', '~> 0.1.3'
+depends 'fpm-tng', '~> 0.1.4'
 depends 'reprepro', '~> 0.3.0'
 depends 'apt', '>= 1.8.2'
 depends 'ohai'
 depends 'discovery', '>= 0.2.0'
-depends 'lxc'
+depends 'lxc', '~> 1.0.0'
