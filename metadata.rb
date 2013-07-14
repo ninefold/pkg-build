3d9842e577de9167bf72d0faf8eb4f13a0ef864e
Chris Roberts <chrisroberts.code@gmail.com>
2013-07-13 17:31:23 -0700

    Only recommend reprepro. We don't want it by default.

--- metadata.rb
+++ metadata.rb
@@ -9,7 +9,7 @@ supports 'ubuntu'
 
 depends 'builder'
 depends 'fpm-tng', '~> 0.1.4'
-depends 'reprepro', '~> 0.3.0'
+recommends 'reprepro', '~> 0.3.0'
 depends 'apt', '>= 1.8.2'
 depends 'ohai'
 depends 'discovery', '>= 0.2.0'
