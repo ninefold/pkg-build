159b9dfa47ff6a7cdd1d300f2f46867bd848d2df
Aaron Baer <aaron@hw-ops.com>
2014-01-21 16:42:33 -0800

    update to latest rubygems or attribute set version

--- attributes/ruby.rb
+++ attributes/ruby.rb
@@ -3,6 +3,7 @@ default[:pkg_build][:ruby][:versions] = []
 default[:pkg_build][:ruby][:version] = '1.9.3'
 default[:pkg_build][:ruby][:patchlevel] = 'p392'
 default[:pkg_build][:ruby][:install_prefix] = '/usr/local'
+default[:pkg_build][:ruby][:rubygems][:version] = 'latest' # or set to rubygems version
 default[:pkg_build][:ruby][:suffix_version] = false
 default[:pkg_build][:ruby][:package_dependencies] = %w(
   openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev 
